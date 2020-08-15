c1dad4d22f35bb862bbad7e9b4d075370382745b
Christie Adams <classee40@hotmail.com>
2020-08-15 12:20:01 -0400

    Updates

---
+++ precache-manifest.2add638ecdff47d263c2ffbd64158177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b224777c7207714f5bc34391c9e4b4b0",
+    "url": "/lingsCars/index.html"
+  },
+  {
+    "revision": "802b8039e0694bbbe6d2",
+    "url": "/lingsCars/static/css/main.0a83dc05.chunk.css"
+  },
+  {
+    "revision": "2b431ee32bd348f7f5db",
+    "url": "/lingsCars/static/js/2.b2cd828f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lingsCars/static/js/2.b2cd828f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802b8039e0694bbbe6d2",
+    "url": "/lingsCars/static/js/main.c8828e7c.chunk.js"
+  },
+  {
+    "revision": "22def6110b5c8fb9947f",
+    "url": "/lingsCars/static/js/runtime-main.11b70f2e.js"
+  }
+]);
